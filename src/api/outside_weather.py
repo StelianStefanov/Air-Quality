a748e032aa7c3fddbea521eaab75ca827d9582ed
Stely Stefanov <stefanov.stely@gmail.com>
2026-05-13 16:45:33 +0300

    Exception handling for the outside weather API call

--- src/api/outside_weather.py
+++ src/api/outside_weather.py
@@ -3,28 +3,46 @@ import json
 import requests
 import time
 
+from requests.exceptions import HTTPError, Timeout, ConnectionError, RequestException
+from src.logger import Logger
 from pathlib import Path
 from dotenv import load_dotenv
 
 class CurrentWeather():
-    def __init__(self):
+    def __init__(self, main_logger: Logger):
         load_dotenv()
+
+        self.main_logger = main_logger
         self.CURRENT_WEATHER_API = os.getenv("CURRENT_WEATHER_API")
         self.CACHE_FILE = "src/api/weather_cache.json"
         self.CACHE_TIME_LIMIT = 1800
 
     
     def _get_current_weather_data(self):
-        response = requests.get(self.CURRENT_WEATHER_API)
-        response.raise_for_status()
+        try:
+            response = requests.get(self.CURRENT_WEATHER_API, timeout=5)
+            response.raise_for_status()
 
-        data = response.json()
-        weather_items = data.get("weather") or []
-        weather_main = weather_items[0].get("main") if weather_items else None
+            data = response.json()
+            weather_items = data.get("weather") or []
+            weather_main = weather_items[0].get("main") if weather_items else None
+
+            return {
+                "temp": data["main"]["temp"],
+                "weather": weather_main,
+            }
+        except Timeout as time_out:
+            self.main_logger.exception(time_out)
+        except ConnectionError as connection_error:
+            self.main_logger.exception(connection_error)
+        except HTTPError as http_err:
+            self.main_logger.error(f"HTTP error: {http_err} Status code: {response.status_code}")
+        except Exception as e:
+            self.main_logger.exception(e)
 
         return {
-            "temp": data["main"]["temp"],
-            "weather": weather_main,
+            "temp": 0,
+            "weather": "Unkown",
         }
 
     
